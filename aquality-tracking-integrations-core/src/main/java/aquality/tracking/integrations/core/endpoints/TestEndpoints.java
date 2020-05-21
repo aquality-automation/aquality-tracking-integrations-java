ad0df65edc91ae2df1ec88e8d09e0d3bd60ca47d
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-05-21 11:31:07 +0300

    Add method to find test

--- aquality-tracking-integrations-core/src/main/java/aquality/tracking/integrations/core/endpoints/TestEndpoints.java
+++ aquality-tracking-integrations-core/src/main/java/aquality/tracking/integrations/core/endpoints/TestEndpoints.java
@@ -3,13 +3,27 @@ package aquality.tracking.integrations.core.endpoints;
 import aquality.tracking.integrations.core.models.Suite;
 import aquality.tracking.integrations.core.models.Test;
 import aquality.tracking.integrations.core.utilities.JsonMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestEndpoints extends AqualityTrackingEndpoints {
 
-    private static final String CREATE_OR_UPDATE_ENDPOINT = "/api/test";
+    private static final String TEST_ENDPOINT = "/api/test";
+
+    public List<Test> findTest(final String name) {
+        Map<String, String> params = new HashMap<String, String>() {{
+            put("name", name);
+            put("project_id", String.valueOf(CONFIG.getProjectId()));
+        }};
+
+        URI uri = buildURI(TEST_ENDPOINT, params);
+        String response = getHttpClient().sendGET(uri, getHeaders());
+        return JsonMapper.mapStringContent(response, new TypeReference<List<Test>>() {});
+    }
 
     public Test createTest(final String name, final List<Suite> suites) {
         return createOrUpdateTest(null, name, suites);
@@ -26,7 +40,7 @@ public class TestEndpoints extends AqualityTrackingEndpoints {
         test.setName(name);
         test.setSuites(suites);
 
-        URI uri = buildURI(CREATE_OR_UPDATE_ENDPOINT);
+        URI uri = buildURI(TEST_ENDPOINT);
         String response = getHttpClient().sendPOST(uri, getHeaders(), JsonMapper.getJson(test));
         return JsonMapper.mapStringContent(response, Test.class);
     }
